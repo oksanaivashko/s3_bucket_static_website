6a22a425f9cd2522b9660dec603442393d6ab766
oksanaivashko <oksivashko13@gmail.com>
2023-07-12 13:14:04 -0500

    added s3 bucket hosting static website with tf

---
+++ S3_Bucket/s3_bucket.tf
@@ -0,0 +1,139 @@
+provider "aws" {
+  region = var.region
+}
+
+terraform {
+  required_version = var.tf_version
+  required_providers {
+    aws = {
+      source  = var.aws_source
+      version = var.aws_version
+    }
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket         = var.backend
+    region         = var.region
+    key            = var.key_state
+    dynamodb_table = var.state_lock
+  }
+}
+
+resource "aws_s3_bucket" "static_website" {
+  bucket = var.bucket_name
+  tags = {
+    Name        = var.bucket_name
+    Environment = var.env
+  }
+}
+# ----- Static website configuration ---
+
+resource "aws_s3_bucket_website_configuration" "static_website_configuration" {
+  bucket = aws_s3_bucket.static_website.id
+
+  index_document {
+    suffix = var.index.html
+  }
+
+  error_document {
+    key = var.error.html
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
+
+# ------ S3 Bucket Policies -------
+
+resource "aws_s3_bucket_policy" "static_website_policy" {
+  bucket = aws_s3_bucket.static_website.id
+
+  policy = jsonencode({
+    
+
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::demo-s3-bucket-test-oksana-tf/*"
+            ]
+        }
+    ]
+
+  })
+}
+
+
+resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# ----- cloudFront distribution with the static website ------
+
+resource "aws_cloudfront_distribution" "static_website_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.static_website.bucket_domain_name
+    origin_id   = aws_s3_bucket.static_website.id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = var.index.html
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.static_website.id
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = var.acm_arn
+    ssl_support_method  = "sni-only"
+  }
+}
---
+++ S3_Bucket/variables.tf
@@ -0,0 +1,61 @@
+variable "region" {
+  default = ""
+  type = string
+}
+
+variable "tf_version" {
+  default = ""
+  type = string
+}
+
+variable "aws_source" {
+  default = ""
+  type = string
+}
+
+variable "aws_version" {
+  default = ""
+  type = string
+}
+
+variable "backend" {
+  default = ""
+  type = string
+}
+
+variable "key_state" {
+  default = ""
+  type = string
+}
+
+variable "state_lock" {
+  default = ""
+  type = string 
+}
+
+variable "bucket_name" {
+  default = ""
+  type = string
+}
+
+variable "env" {
+  default = ""
+  type = string
+}
+
+variable "index.html" {
+  default = "index.html"
+  type = string
+}
+
+variable "error.html" {
+  default = "error.html"
+  type = string
+}
+
+# ---- certificate arn ---
+
+variable "acm_arn" {
+  default = ""
+  type = string
+}
