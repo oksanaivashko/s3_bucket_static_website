2ed680f49427efa0edd441bd7f33bdd9af9af2f7
oksanaivashko <oksivashko13@gmail.com>
2023-07-12 17:12:46 -0500

    removed variables

--- S3_Bucket/variables.tf
+++ S3_Bucket/variables.tf
@@ -3,21 +3,6 @@ variable "region" {
   type = string
 }
 
-variable "tf_version" {
-  default = ""
-  type = string
-}
-
-variable "aws_source" {
-  default = ""
-  type = string
-}
-
-variable "aws_version" {
-  default = ""
-  type = string
-}
-
 variable "bucket_name" {
   default = ""
   type = string
@@ -25,16 +10,6 @@ variable "bucket_name" {
 
 variable "env" {
   default = ""
-  type = string
-}
-
-variable "index_html" {
-  default = "index.html"
-  type = string
-}
-
-variable "error_html" {
-  default = "error.html"
   type = string
 }
 
