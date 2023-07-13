5f14d9b3f0907bbbc43f96574fa10964e3ff0b88
oksanaivashko <oksivashko13@gmail.com>
2023-07-13 16:45:51 -0500

    testing

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -80,9 +80,9 @@ resource "aws_s3_bucket_policy" "static_website_policy" {
 resource "aws_s3_bucket_public_access_block" "bucket_public_access_block" {
   bucket = aws_s3_bucket.static_website.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
