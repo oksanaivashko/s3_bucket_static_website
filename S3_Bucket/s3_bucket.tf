a8ffc7f0046ce7b8161f3e25b21a91c9179fe813
oksanaivashko <oksivashko13@gmail.com>
2023-07-18 12:50:18 -0500

    fixed error

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -120,11 +120,10 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   min_ttl                = 0
   default_ttl            = 3600
   max_ttl                = 86400
+ }
   tags {
     Environment = var.env
    }
- }
-
   price_class = "PriceClass_100"
 
   restrictions {
