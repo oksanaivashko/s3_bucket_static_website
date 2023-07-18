e0caedc3135bf57077894dadf955aca26e9b01ca
oksanaivashko <oksivashko13@gmail.com>
2023-07-18 17:10:37 -0500

    fixing error

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -113,7 +113,7 @@ resource "aws_acm_certificate_validation" "www_oksanai_com" {
 resource "aws_route53_record" "oksanai_com" {
   zone_id = "Z0480476I3YP7F1IGH87"
   name    = "oksanai.com"
-  type    = "CNAME"
+  type    = "A"
   ttl     = 300
   records = aws_acm_certificate.certificate.domain_validation_options[*].resource_record_value
   lifecycle {
@@ -124,7 +124,7 @@ resource "aws_route53_record" "oksanai_com" {
 resource "aws_route53_record" "www_oksanai_com" {
   zone_id = "Z0480476I3YP7F1IGH87"
   name    = "www.oksanai.com"
-  type    = "CNAME"
+  type    = "A"
   ttl     = 300
   records = aws_acm_certificate.certificate.domain_validation_options[*].resource_record_value
   lifecycle {
