d929893ece436b3bc4b6f4c740d99ac5a3aab527
oksanaivashko <oksivashko13@gmail.com>
2023-07-18 12:49:18 -0500

    fixed error

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -93,7 +93,7 @@ resource "aws_acm_certificate" "certificate" {
 resource "aws_cloudfront_distribution" "s3_distribution" {
   origin {
     domain_name              = aws_s3_bucket.static_website.bucket_regional_domain_name
-    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+   # origin_access_control_id = aws_cloudfront_origin_access_control.default.id
     origin_id                = local.s3_origin_id
   }
 
