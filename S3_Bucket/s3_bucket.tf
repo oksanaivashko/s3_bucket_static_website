0d9d1f7ef8d1e910722e9c74224965a47f5f89a0
oksanaivashko <oksivashko13@gmail.com>
2023-07-18 17:53:07 -0500

    added bucket acccess back

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -38,14 +38,14 @@ website {
 
 }
 
-#resource "aws_s3_bucket_public_access_block" "public_acl" {
-#  bucket = aws_s3_bucket.static_website.id
-#
-#  block_public_acls       = true
-#  block_public_policy     = true
-#  ignore_public_acls      = true
-#  restrict_public_buckets = true
-#}
+resource "aws_s3_bucket_public_access_block" "public_acl" {
+  bucket = aws_s3_bucket.static_website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 
 # ------ S3 Bucket Policies -------
