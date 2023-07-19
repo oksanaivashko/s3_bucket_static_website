a710967f3e9869affd43114b328047c43703d03c
oksanaivashko <oksivashko13@gmail.com>
2023-07-18 21:17:42 -0500

    fixed error

--- S3_Bucket/s3_bucket.tf
+++ S3_Bucket/s3_bucket.tf
@@ -148,7 +148,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
   default_cache_behavior {
   allowed_methods  = ["GET", "HEAD"]
   cached_methods   = ["GET", "HEAD"]
-  #target_origin_id = local.s3_origin_id
+  target_origin_id = local.s3_origin_id
 
   forwarded_values {
     query_string = false
